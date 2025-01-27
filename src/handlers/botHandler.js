cf7c29b6e698439f973b0d91646c68d231fd4180
Ariel2711 <arielnaviandanaputra@gmail.com>
2025-01-27 21:15:13 +0700

    fix bug bot

--- src/handlers/botHandler.js
+++ src/handlers/botHandler.js
@@ -2,9 +2,6 @@ const botHandler = (io, botService) => {
     io.on('connection', (socket) => {
         socket.on('message', async (data) => {
             const response = await botService.processMessage(data)
-            if (response) {
-                socket.emit('message', response)
-            }
         })
     })
 }
